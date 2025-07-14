3a6b64d467ab67b0b708f2338efeb84024419b1a
Aditya Sharma <104629524+imadityaxd@users.noreply.github.com>
2025-07-14 09:27:20 +0530

    c++stl

---
+++ c++STL.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int add(int a, int b){
+    return a+b;
+}
+int main(){
+    int num;
+    cout << "enter a number: ";
+    cin >> num ;
+    cout<< add(num, 4);
+    return 0;
+}
